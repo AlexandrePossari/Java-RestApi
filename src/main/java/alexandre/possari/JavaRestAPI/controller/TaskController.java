684454412df47c6a8b08b1a20502f94c607fdbcd
Alexandre Possari <ac.possari@gmail.com>
2024-07-04 19:40:45 -0300

    feat: create task via post

--- src/main/java/alexandre/possari/JavaRestAPI/controller/TaskController.java
+++ src/main/java/alexandre/possari/JavaRestAPI/controller/TaskController.java
@@ -1,13 +1,13 @@
 package alexandre.possari.JavaRestAPI.controller;
 
 import alexandre.possari.JavaRestAPI.domain.Task;
+import alexandre.possari.JavaRestAPI.requests.TaskPostRequestBody;
 import alexandre.possari.JavaRestAPI.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,5 +23,10 @@ public class TaskController {
     public ResponseEntity<String> list() {
         log.info("teste");
         return ResponseEntity.ok("teste");
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> save(@RequestBody TaskPostRequestBody taskPostRequestBody) {
+        return new ResponseEntity<>(taskService.save(taskPostRequestBody), HttpStatus.CREATED);
     }
 }
--- src/main/java/alexandre/possari/JavaRestAPI/service/TaskService.java
+++ src/main/java/alexandre/possari/JavaRestAPI/service/TaskService.java
@@ -2,6 +2,7 @@ package alexandre.possari.JavaRestAPI.service;
 
 import alexandre.possari.JavaRestAPI.domain.Task;
 import alexandre.possari.JavaRestAPI.repository.TaskRepository;
+import alexandre.possari.JavaRestAPI.requests.TaskPostRequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,13 @@ public class TaskService {
 
     public List<Task> listAll(){
         return taskRepository.findAll();
+    }
+
+    public Task save(TaskPostRequestBody taskPostRequestBody) {
+        return taskRepository.save(Task.builder().title(taskPostRequestBody.getTitle())
+                        .description(taskPostRequestBody.getDescription())
+                        .dueDate(taskPostRequestBody.getDueDate())
+                        .status(taskPostRequestBody.getStatus())
+                .build());
     }
 }
